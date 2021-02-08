74cb93276c02211d3114be7b4a8dced18ed656dc
Sofia Hervéus <sofia@herveus.se>
2021-02-08 11:45:55 +0100

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+window.onscroll = funtion() 
+    {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction(){
+    if (window.pageYOffset >= sticky){
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
